097fd476a2035e3c2629126e3b73b3f52faf9162
Ankit Sethi <61274953+4nkitsethi@users.noreply.github.com>
2023-06-15 14:57:03 +0530

    Responsiveness

--- resources/js/Layouts/WebLayout.vue
+++ resources/js/Layouts/WebLayout.vue
@@ -133,18 +133,11 @@
       </div>
       <ul class="navbar-nav secondary-menu flex-row">
         <li class="nav-item">
-          <a class="nav-link dark-mode-icon" href="javascript:;">
-            <div class="mode-icon">
-              <i class="bi bi-moon"></i>
-            </div>
-          </a>
-        </li>
-        <li class="nav-item">
           <a class="nav-link" href="search.html"><i class="bi bi-search"></i></a>
         </li>
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <inertia-link class="nav-link" :href="route('salonpe.wishlist')"><i class="bi bi-suit-heart"></i></inertia-link>
-        </li>
+        </li> -->
         <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight">
           <a class="nav-link position-relative" href="javascript:;">
             <div class="cart-badge">8</div>
